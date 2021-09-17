bd88b1d59b7dc3d4859c32c66ab8aae63c1a255b
Francisco Tierry Barros Oliveira <francisco.barros@lead.fortaleza>
2021-09-17 15:52:52 -0300

    configuração do icone e splash screen

--- App.tsx
+++ App.tsx
@@ -1,7 +1,12 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import SplashScreen from 'react-native-splash-screen';
 import { MovieList } from './src/screens/MovieList';
 
 export default function App() {
+  useEffect(() => {
+    SplashScreen.hide()
+  })
+
   return (
     <MovieList />
   );
--- src/screens/MovieList/index.tsx
+++ src/screens/MovieList/index.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
-import {View,Text} from 'react-native';
+import {View,Text, StatusBar} from 'react-native';
 
 import {styles} from './styles';
 
 export function MovieList() {
   return (
     <View style={styles.container}>
+      <StatusBar barStyle="dark-content" backgroundColor="transparent" translucent/>
       <Text>Hello World</Text>
     </View>
   )
